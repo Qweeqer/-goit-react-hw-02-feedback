ea334bad25c13dfccdd63bc00649c2f144e004d7
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-08-12 18:44:52 +0300

    Update FeedbackOptions.jsx

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import '../FeedbackOptions/FeedbackOptions.module.css';
 
 const FeedbackOptions = ({ handleGood, handleNeutral, handleBad }) => (
-  <div className="Feedback__controls">
+  <div className="feedbackBtn">
     <button onClick={handleGood}>Good</button>
     <button onClick={handleNeutral}>Neutral</button>
     <button onClick={handleBad}>Bad</button>
   </div>
 );
 
+Notification.propTypes = {
+  handleGood: PropTypes.string,
+  handleNeutral: PropTypes.string,
+  handleBad: PropTypes.string,
+};
 
 export default FeedbackOptions;
