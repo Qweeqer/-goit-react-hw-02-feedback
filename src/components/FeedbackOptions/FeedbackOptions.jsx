a8d600f3720d2c2be65751d25f4a18c8468770d0
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-08-15 11:09:01 +0300

    Update FeedbackOptions.jsx

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -3,18 +3,29 @@ import PropTypes from 'prop-types';
 
 import '../FeedbackOptions/FeedbackOptions.module.css';
 
-const FeedbackOptions = ({ handleGood, handleNeutral, handleBad }) => (
-  <div className="feedbackBtn">
-    <button onClick={handleGood}>Good</button>
+const FeedbackOptions = ({ options, onBtnClick }) => {
+  return (
+    <div className="feedbackBtn">
+      {options.map(key => {
+        return (
+          <button key={key} type="button" onClick={() => onBtnClick(key)}>
+            {key}
+          </button>
+        );
+      })}
+      {/* <button onClick={handleGood}>Good</button>
     <button onClick={handleNeutral}>Neutral</button>
-    <button onClick={handleBad}>Bad</button>
-  </div>
-);
+    <button onClick={handleBad}>Bad</button> */}
+    </div>
+  );
+};
 
 FeedbackOptions.propTypes = {
-  handleGood: PropTypes.any,
-  handleNeutral: PropTypes.any,
-  handleBad: PropTypes.any,
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onBtnClick: PropTypes.func.isRequired,
+  // handleGood: PropTypes.any,
+  // handleNeutral: PropTypes.any,
+  // handleBad: PropTypes.any,
 };
 
 export default FeedbackOptions;
